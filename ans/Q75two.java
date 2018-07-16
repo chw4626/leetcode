9921df95d9d1b3cdd4baf385ba67b7edc78c2df2
chw4626 <664153604@qq.com>
2018-07-16 10:09:05 +0800

    7.16

---
+++ ans/Q75two.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        for (int i = 0; i < nums.length; i ++){
+            if(nums[i]==0){
+                red++;
+            }
+            else if(nums[i]==1){
+                white++;
+            }
+            else if(nums[i]==2){
+                blue++;
+            }
+        }
+        for (int i = 0; i < red; i ++){
+            nums[i] = 0;
+        }
+        for (int i = red; i < white+red; i ++){
+            nums[i] = 1;
+        }
+        for (int i = white+red; i < nums.length; i ++ ){
+            nums[i] = 2;
+        }
+    }
+}
