d92f855fa20665ce95b237179ad1f67be969dbad
chw4626 <664153604@qq.com>
2018-07-31 14:29:31 +0800

    7.31

---
+++ ans/Q3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0;
+        if (s.length()==0){
+            return 0;
+        } 
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i =0 , j = 0; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            res = Math.max(res,i-j+1);
+        }
+        return res;
+    }
+}
