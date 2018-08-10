dcd93d21028dd461452160c36540b91dc544ec30
chw4626 <664153604@qq.com>
2018-08-10 13:34:06 +0800

    8.10

---
+++ ans/Q61.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null)
+		return head;
+	
+	ListNode copyHead = head;
+	
+	int len = 1;
+	while (copyHead.next != null) {
+		copyHead = copyHead.next;
+		len++;
+	}
+	
+	copyHead.next = head;
+	
+	for (int i = len - k % len; i > 1; i--)
+		head = head.next;
+
+	copyHead = head.next;
+	head.next = null;
+
+	return copyHead;
+}
+}
