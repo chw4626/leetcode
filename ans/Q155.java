6763eefa11b439c51d46d20c9bf083e54cca6a9b
chw4626 <chang.haow@husky.neu.edu>
2019-01-07 02:40:52 -0500

    1.7

---
+++ ans/Q155.java
@@ -0,0 +1,33 @@
+class MinStack {
+int min = Integer.MAX_VALUE;
+Stack<Integer> stack;
+//Stack<Integer> stack = new Stack<Integer>();
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if(x <= min){
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.peek() == min){
+            stack.pop();
+            min=stack.pop();
+        }else
+            stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
