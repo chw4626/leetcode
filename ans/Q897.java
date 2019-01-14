707d2e5a746285afed047c75521682f27f3682a8
chw4626 <changhaowen4626@gmail.com>
2019-01-14 02:34:38 -0500

    1.13

---
+++ ans/Q897.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode prev=null, head=null;
+    public TreeNode increasingBST(TreeNode root) {
+        
+        if(root == null)
+            return null;
+        
+        increasingBST(root.left);  
+        if(prev!=null) { 
+        	root.left=null; // we no  longer needs the left  side of the node, so set it to null
+        	prev.right=root; 
+        }
+        if(head==null) head=root; // record the most left node as it will be our root
+        prev=root; //keep track of the prev node
+        increasingBST(root.right); 
+        return head;
+
+    }
+}
