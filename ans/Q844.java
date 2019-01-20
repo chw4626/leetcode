1f293e4fe8776fe0de967b70bdc4675f35a894ab
chw4626 <changhaowen4626@gmail.com>
2019-01-20 14:20:25 -0500

    1.20

---
+++ ans/Q844.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return getString(S).equals(getString(T));
+    }
+    private String getString(String str) {
+        int n=str.length(), count=0;
+        String result="";
+        for(int i=n-1; i>=0; i--) {
+            char ch=str.charAt(i);
+            if(ch=='#') 
+                count++;
+            else {
+                if(count>0)
+                    count--;
+                else {
+                    result+=ch;
+                }                     
+            }
+        }
+        return result;
+    }
+}
